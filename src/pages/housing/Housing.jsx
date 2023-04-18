e3e302e4e9cc5b7b58d0021941ae11d10209cf84
TristanGeh <109686323+TristanGeh@users.noreply.github.com>
2023-04-18 03:52:04 +0200

    fix housing.jsx eslint issues

--- src/pages/housing/Housing.jsx
+++ src/pages/housing/Housing.jsx
@@ -8,22 +8,39 @@ import logements from '../../logements.json'
 import arrow from '../../assets/arrow.png'
 import housingArrow from '../../assets/housing_arrow.png'
 
-import "./housing.scss"
+import './housing.scss'
 
 function Housing() {
-  const { id } = useParams();
-  const housing = logements.find((housing) => housing.id === id);
+  const { id } = useParams()
+  const housing = logements.find((housing) => housing.id === id)
 
-  if(!housing){
+  if (!housing) {
     return <Error />
   }
 
   return (
-    <section className='housing-main'>
+    <section className="housing-main">
       <PageTitle title={`${housing.title} - Kasa`} />
-      <SlideShow images={housing.pictures} arrow={housingArrow} title={housing.title} />
-      <Info title={housing.title} location={housing.location} name={housing.host.name} picture={housing.host.picture} id={housing.id} tags={housing.tags} rating={housing.rating} />
-      <Collapse contentType={["equipements", "description"]} data={housing} arrow={arrow} customClass="collapse-housing"/>
+      <SlideShow
+        images={housing.pictures}
+        arrow={housingArrow}
+        title={housing.title}
+      />
+      <Info
+        title={housing.title}
+        location={housing.location}
+        name={housing.host.name}
+        picture={housing.host.picture}
+        id={housing.id}
+        tags={housing.tags}
+        rating={housing.rating}
+      />
+      <Collapse
+        contentType={['equipements', 'description']}
+        data={housing}
+        arrow={arrow}
+        customClass="collapse-housing"
+      />
     </section>
   )
 }
